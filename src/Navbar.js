05b8a0b3a2c61dc412dab8335d386fe319aee9d0
Shankhadeep Bhadra <shankhadeepraj@gmail.com>
2019-11-15 19:33:01 +0530

    add snackbar and style select

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
 import Slider from "rc-slider";
+import { Select, IconButton } from "@material-ui/core";
+import { MenuItem } from "@material-ui/core";
+import Snackbar from "@material-ui/core/Snackbar";
+import CloseIcon from "@material-ui/icons/Close";
+
 import "rc-slider/assets/index.css";
 import "./Navbar.css";
 
 class Navbar extends Component {
+  state = { format: "hex", open: false };
+
+  handleFormatChange = e => {
+    this.setState({ format: e.target.value, open: true });
+    this.props.handleChange(e.target.value);
+  };
+
+  closeSnackbar = () => {
+    this.setState({ open: false });
+  };
+
   render() {
     const { level, changeLevel } = this.props;
+    const { format } = this.state;
     return (
       <header className="Navbar">
         <div className="logo">
@@ -23,6 +40,37 @@ class Navbar extends Component {
             />
           </div>
         </div>
+        <div className="select-container">
+          <Select value={format} onChange={this.handleFormatChange}>
+            <MenuItem value="hex">HEX - #ffffff</MenuItem>
+            <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+            <MenuItem value="rgba">RGBA - rgb(255,255,255,1.0)</MenuItem>
+          </Select>
+        </div>
+        <Snackbar
+          anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+          open={this.state.open}
+          autoHideDuration={3000}
+          message={
+            <span id="message-id">
+              Format Changed To {format.toUpperCase()}
+            </span>
+          }
+          ContentProps={{
+            "aria-describedby": "message-id"
+          }}
+          onClose={this.closeSnackbar}
+          action={[
+            <IconButton
+              onClick={this.closeSnackbar}
+              color="inherit"
+              key="close"
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+          ]}
+        />
       </header>
     );
   }
--- src/Palette.js
+++ src/Palette.js
@@ -5,22 +5,30 @@ import ColorBox from "./ColorBox";
 import "./Palette.css";
 
 class Palette extends Component {
-  state = { level: 500 };
+  state = { level: 500, format: "hex" };
 
   changeLevel = level => {
     this.setState({ level });
   };
 
+  changeFormat = val => {
+    this.setState({ format: val });
+  };
+
   render() {
     const { colors } = this.props.palette;
-    const { level } = this.state;
+    const { level, format } = this.state;
 
     const ColorBoxes = colors[level].map(color => (
-      <ColorBox background={color.hex} name={color.name} />
+      <ColorBox background={color[format]} name={color.name} />
     ));
     return (
       <div className="Palette">
-        <Navbar level={level} changeLevel={this.changeLevel} />
+        <Navbar
+          level={level}
+          changeLevel={this.changeLevel}
+          handleChange={this.changeFormat}
+        />
         {/* navbar will go here */}
         <div className="Palette-colors">{ColorBoxes}</div>
         {/* footer will go here */}
